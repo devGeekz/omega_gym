025c3e78784c4c9edb77dc30d34080d58ba974c1
berghuis18 <maincoder69@gmail.com>
2025-11-20 09:56:08 +0100

    fklnanflknmadf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Home() {
   return (
     <div>
       home page
       <div>
+        <Button variant={"outline"}>test me</Button>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+            <CardAction>Card Action</CardAction>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur,
         molestiae? Facere repellat nulla tenetur illo cum voluptatibus, dolore
         nesciunt rem! Quae doloribus ab animi assumenda doloremque placeat
