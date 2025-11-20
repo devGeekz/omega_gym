b57cc4e1562e1d9b7b7e1fb61b2dc0e07a8968e2
berghuis18 <maincoder69@gmail.com>
2025-11-20 09:20:09 +0100

    terth

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import Link from 'next/link';
+
+const Navbar= () => {
+    return (
+        <nav>
+            {
+                /* Navbar content goes here */
+            }
+        </nav>
+    );
+};
+
+export default Navbar;
