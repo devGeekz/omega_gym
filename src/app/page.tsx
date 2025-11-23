33b309acbad37e3a8d41af615c54e6d4d7265445
PierreBond <cyberkeen@proton.me>
2025-11-23 19:09:37 +0100

    new hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { GooeyText } from "@/components/home/gooey-text-morphing";
 import { Hero } from "@/components/home/Hero";
 import { HomePageSections } from "@/components/home/HomepageSections";
+import { HeroSection } from "@/components/home/hero-section";
+
 export default function Home() {
   return (
     <div>
-      <div className="pt-24 min-h-screen flex items-center justify-center">
-        {<GooeyText texts={["Omega", "Gym", "Is", "Awesome"]} />}{" "}
+      <div>
+        <HeroSection />
       </div>
       <Hero />
       <HomePageSections />
